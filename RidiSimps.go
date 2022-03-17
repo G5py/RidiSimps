1f27a0465562e2c059df9b01e1493b30cec6e9d6
GSpy <GSpy0231@gmail.com>
2022-03-18 00:39:38 +0900

    feat : login 함수 부분적 구현

--- RidiSimps.go
+++ RidiSimps.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"mime/multipart"
+)
 
 func loginData() (id string, password string) {
 	fmt.Print("ID : ")
@@ -12,6 +16,31 @@ func loginData() (id string, password string) {
 }
 
 func login(id string, password string) {
+
+	// 로그인 request의 body를 작성하는 과정.
+	// POST method, content-type : multipart/form-data
+
+	fieldNames := []string{ // 리디북스 로그인 request의 body field들.
+		"user_id",
+		"password",
+		"cmd",
+		"return_url",
+		"return_query_string",
+		"device_id",
+		"msg"}
+
+	var body bytes.Buffer
+	w := multipart.NewWriter(&body)
+
+	w.WriteField(fieldNames[0], id)
+	w.WriteField(fieldNames[1], password)
+	w.WriteField(fieldNames[2], "login")
+
+	for _, fn := range fieldNames[3:] {
+		w.CreateFormField(fn)
+	}
+
+	w.Close()
 
 }
 
