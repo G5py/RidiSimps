8a6614bb80cfb64dd53d9fb2c8e58869bfabefd9
GSpy <GSpy0231@gmail.com>
2022-03-29 23:02:31 +0900

    feat : 크롤링 과정 일부 구현, 의존성 추가

--- RidiSimps.go
+++ RidiSimps.go
@@ -5,8 +5,13 @@ import (
 	"fmt"
 	"mime/multipart"
 	"net/http"
+	"net/url"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
+// 콘솔창으로 id와 비밀번호를 입력받습니다.
 func loginData() (id string, password string) {
 	fmt.Print("ID : ")
 	fmt.Scanln(&id)
@@ -49,11 +54,50 @@ func login(id string, password string) (*http.Response, error) {
 }
 
 func main() {
+
+	// 사용자로부터 id와 비밀번호를 입력받음.
 	id, password := loginData()
 
-	resp, err := login(id, password)
+	// id와 비밀번호로 ridibooks에 로그인.
+	loginResp, err := login(id, password)
 	if err != nil {
 		fmt.Println(err, ": login failed")
+		panic(err)
+	}
+
+	// 반복적인 get 호출을 위해 client 선언.
+	client := &http.Client{}
+	ridiUrl, err := url.Parse("https://ridibooks.com")
+	if err != nil {
+		fmt.Println(err, ": url parse error")
+		panic(err)
+	}
+
+	// 로그인 세션을 유지하기 위해 client에 쿠키를 설정.
+	client.Jar.SetCookies(ridiUrl, loginResp.Cookies())
+	loginResp.Body.Close()
+
+	historyUri := "https://ridibooks.com/order/history?page="
+
+	const maxTries int = 100
+	for i := 1; i <= maxTries; i++ {
+		historyResp, err := client.Get(historyUri + strconv.Itoa(i))
+		if err != nil {
+			fmt.Println(err, ": history response error")
+			panic(err)
+		}
+
+		html, err := goquery.NewDocumentFromResponse(historyResp)
+		if err != nil {
+			fmt.Println(err, ": goquery parsing error")
+			panic(err)
+		}
+
+
+		buyHistoryTable := html.Find("#page_buy_history>table>tbody")
+		if buyHistoryTable.
+		 := buyHistoryTable.Find("tr.detail_link js_rui_detail_link")
+
 	}
 
 }
